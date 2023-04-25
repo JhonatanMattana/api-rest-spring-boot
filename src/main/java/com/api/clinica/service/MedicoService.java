e3f21624e85f40e407574195899520958370ac5e
Jhonatan Mattana <jhonatan_mattana@outlook.com>
2023-04-25 18:06:17 -0300

    criado script sql que cria tabela medicos

--- src/main/java/com/api/clinica/service/MedicoService.java
+++ src/main/java/com/api/clinica/service/MedicoService.java
@@ -2,6 +2,7 @@ package com.api.clinica.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.api.clinica.model.Medico;
 import com.api.clinica.repository.MedicoRepository;
@@ -12,6 +13,7 @@ public class MedicoService {
 	@Autowired
 	private MedicoRepository medicoRepository;
 	
+	@Transactional
 	public void save(Medico medico) {
 		medicoRepository.save(medico);
 	}
