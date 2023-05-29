715d5b641a2023f39932ea261dbd5e2233c2b5d5
Jhonatan Mattana <kaverinhaa@hotmail.com>
2023-05-29 10:57:43 -0300

    Criado class AutenticacaoService

--- src/main/java/com/api/clinica/repository/UsuarioRepository.java
+++ src/main/java/com/api/clinica/repository/UsuarioRepository.java
@@ -1,9 +1,12 @@
 package com.api.clinica.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.api.clinica.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+	UserDetails findByLogin(String username);
 
 }
