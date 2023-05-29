c5b5a14eea7439a3661e4bb5d3ebd6b10c270a55
Jhonatan Mattana <kaverinhaa@hotmail.com>
2023-05-29 10:27:20 -0300

    criado entidade Usuario

---
+++ src/main/java/com/api/clinica/model/Usuario.java
@@ -0,0 +1,28 @@
+package com.api.clinica.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Data
+@Getter
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String login;
+	private String senha;
+}
