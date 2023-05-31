04305c9ad019b0557c4a12d553b11e2e5e3b69eb
Jhonatan Mattana <kaverinhaa@hotmail.com>
2023-05-30 22:58:25 -0300

    Recuperando o token.

--- src/main/java/com/api/clinica/infra/security/SecurityFilter.java
+++ src/main/java/com/api/clinica/infra/security/SecurityFilter.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -17,7 +16,20 @@ public class SecurityFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
+		var tokenJWT = recuperarToken(request);
 		
+		System.err.println(tokenJWT);
+		
+		filterChain.doFilter(request, response);
+	}
+
+	private String recuperarToken(HttpServletRequest request) {
+		var authorizationHeader = request.getHeader("Authorization");
+		
+		if (authorizationHeader == null)
+			throw new RuntimeException("Token JWT não enviado no cabeçalho Authorizaton.");
+		
+		return authorizationHeader.replace("Bearer", "");
 	}
 	
 }
