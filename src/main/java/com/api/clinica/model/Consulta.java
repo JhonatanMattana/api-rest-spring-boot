3d3e6bcc3219801acf0d43996f1a0c98bdfdfd71
Jhonatan Mattana <kaverinhaa@hotmail.com>
2023-09-26 22:13:59 -0300

    Correção na anotação da entidade e do script SQL

--- src/main/java/com/api/clinica/model/Consulta.java
+++ src/main/java/com/api/clinica/model/Consulta.java
@@ -2,12 +2,14 @@ package com.api.clinica.model;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -16,6 +18,8 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "consultas")
+@Entity(name = "Consulta")
 @EqualsAndHashCode(of = "id")
 public class Consulta {
 	
